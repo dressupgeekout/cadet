cef2c97f17f0ff4e7e4b9603922d5400844c190f
Christian Koch <cfkoch@sdf.lonestar.org>
2014-10-02 01:03:38 -0700

    Add the templatefile example.

---
+++ example/templatefile/templatefile.lua
@@ -0,0 +1,27 @@
+package.path = "/home/christian/devel/cadet/?.lua;" .. package.path
+local cadet = require("cadet")
+
+App = {
+  index = function(env, headers, query)
+    local res = cadet.response
+
+    view = {
+      title = "Test Application!",
+      question = "What do we want?",
+      answer = "I don't know, but we want it now!",
+    }
+
+    body =
+      cadet.render_file("/home/christian/devel/cadet/example/templatefile/layout.html", view)
+
+    if (body) then
+      cadet.write(body)
+    else
+      cadet.write("um something went wrong\n")
+    end
+
+    return cadet.finish()
+  end,
+}
+
+httpd.register_handler("index", App.index)
